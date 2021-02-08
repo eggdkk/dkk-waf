557aa34e5189a54f1f2c722d09683218da5b62f7
eggdkk <dk891896315@163.com>
2021-02-08 18:47:14 +0800

    mysql

--- redis_get.lua
+++ redis_get.lua
@@ -1,11 +1,22 @@
 require "func"
 require "config"
 local log_path = "/usr/local/openresty/nginx/logs/hack/";
-
 local filename = log_path .. "redis.log";
-
 local h = ngx.resp.get_headers();
 local res = "";
+
+local mysql = require "resty.mysql"
+-- 初始化数据库对象
+local db, err = mysql:new()
+if not db then
+    ngx.say("failed to instantiate mysql: ", err)
+    return
+end
+-- 设置连接超时
+db:set_timeout(1000)
+-- 设置连接最大空闲时间，连接池容量
+db:set_keepalive(10000, 100)
+
 for k, v in pairs(h) do
     local values = ""
     if type(v) == "table" then
@@ -19,13 +30,9 @@ for k, v in pairs(h) do
     if k == "set-cookie" then
         res = res .. k .. "=" .. values .. ";\n"
     end
-    write(filename,res);
+    -- write(filename,res);
 end
-local hs = require('hyperscan')
-local obj = hs.get("test1_obj")
-local ret, id = obj:scan("abcdefghisghk")
-if ret then
-    return ngx.print("matchid:", id)
-else
-    return ngx.print("not match")
-end
+
+local vulnerable_app_session = string.match(res, "vulnerable_app_session=(.-);");
+
+local student_id string.match(res, "student_id=(.-);");
