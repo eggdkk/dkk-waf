347e7fbef588f54703949806c24c51cbf9e1508e
eggdkk <dk891896315@163.com>
2021-02-08 21:12:53 +0800

    rm_redis_0

--- redis_get.lua
+++ redis_get.lua
@@ -2,6 +2,7 @@ require "func"
 require "config"
 local log_path = "/usr/local/openresty/nginx/logs/hack/";
 local filename = log_path .. "redis.log";
+local cookie_file = log_path .. "cookie.log";
 local h = ngx.resp.get_headers();
 local res = "";
 local request_uri = ngx.var.request_uri
@@ -25,59 +26,61 @@ if request_uri == sign_in_url then
     local vulnerable_app_session = string.match(res, "vulnerable_app_session=(.-);");
 
     local student_id string.match(res, "student_id=(.-);");
-    local handler
-    handler = function (session,id)
-        -- do some routine job in Lua just like a cron job
-        if session then
-            return
-        end
-        if id then
-            return
-        end
-        local redis = require "resty.redis"
-        local red = redis:new()
-        red:set_timeout(1000) -- 1 sec
 
-        local ok, err = red:connect("127.0.0.1", 6379)
-        if not ok then
-            ngx.log(ngx.ERR,"failed to connect: "..err)
-            return
-        end
-
-        -- 请注意这里 auth 的调用过程
-        local count
-        count, err = red:get_reused_times()
-        if 0 == count then
-            ok, err = red:auth("Ul4YfsfPwRgcDIf5")
-            if not ok then
-                ngx.log(ngx.ERR,"failed to auth: "..err)
-                return
-            end
-        elseif err then
-            ngx.log(ngx.ERR,"failed to get reused times: ".. err)
-            return
-        end
-        ok, err = red:set(tostring(id),tostring(session))
-        if not ok then
-            ngx.log(ngx.ERR,"param is : ".. err);
-            write(filename,id..": "..ngx.md5(session).."\n");
-            return
-        end
-        write(filename, "success\n");
-
-        local ok, err = red:set_keepalive(10000, 100)
-        if not ok then
-            ngx.log(ngx.ERR,"failed to set keepalive: ".. err)
-            return
-        end
-
-        ok, err = red:set(tostring(id),tostring(session))
-        if not ok then
-            ngx.log(ngx.ERR,"param is : ".. err);
-            write(filename,id..": "..ngx.md5(session).."\n");
-            return
-        end
-        write(filename, "success\n");
-    end
-    local ok, err = ngx.timer.at(0.001, handler,vulnerable_app_session,student_id);
+    write(cookie_file,student_id .. ":" .. ngx.md5(vulnerable_app_session).."\n");
+    --local handler
+    --handler = function (session,id)
+    --    -- do some routine job in Lua just like a cron job
+    --    if session then
+    --        return
+    --    end
+    --    if id then
+    --        return
+    --    end
+    --    local redis = require "resty.redis"
+    --    local red = redis:new()
+    --    red:set_timeout(1000) -- 1 sec
+    --
+    --    local ok, err = red:connect("127.0.0.1", 6379)
+    --    if not ok then
+    --        ngx.log(ngx.ERR,"failed to connect: "..err)
+    --        return
+    --    end
+    --
+    --    -- 请注意这里 auth 的调用过程
+    --    local count
+    --    count, err = red:get_reused_times()
+    --    if 0 == count then
+    --        ok, err = red:auth("Ul4YfsfPwRgcDIf5")
+    --        if not ok then
+    --            ngx.log(ngx.ERR,"failed to auth: "..err)
+    --            return
+    --        end
+    --    elseif err then
+    --        ngx.log(ngx.ERR,"failed to get reused times: ".. err)
+    --        return
+    --    end
+    --    ok, err = red:set(tostring(id),tostring(session))
+    --    if not ok then
+    --        ngx.log(ngx.ERR,"param is : ".. err);
+    --        write(filename,id..": "..ngx.md5(session).."\n");
+    --        return
+    --    end
+    --    write(filename, "success\n");
+    --
+    --    local ok, err = red:set_keepalive(10000, 100)
+    --    if not ok then
+    --        ngx.log(ngx.ERR,"failed to set keepalive: ".. err)
+    --        return
+    --    end
+    --
+    --    ok, err = red:set(tostring(id),tostring(session))
+    --    if not ok then
+    --        ngx.log(ngx.ERR,"param is : ".. err);
+    --        write(filename,id..": "..ngx.md5(session).."\n");
+    --        return
+    --    end
+    --    write(filename, "success\n");
+    --end
+    --local ok, err = ngx.timer.at(0.001, handler,vulnerable_app_session,student_id);
 end
