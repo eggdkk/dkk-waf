628738d813d84975160b29eb63daf5dece43ae35
eggdkk <dk891896315@163.com>
2021-02-08 19:06:34 +0800

    redis_init_1

--- redis_get.lua
+++ redis_get.lua
@@ -39,7 +39,7 @@ handler = function (session,id)
 
     local ok, err = red:connect("127.0.0.1", 6379)
     if not ok then
-        ngx.set("failed to connect: ",err)
+        ngx.log(ngx.ERR,"failed to connect: ",err)
         return
     end
 
@@ -49,18 +49,18 @@ handler = function (session,id)
     if 0 == count then
         ok, err = red:auth("Ul4YfsfPwRgcDIf5")
         if not ok then
-            ngx.set("failed to auth: ",err)
+            ngx.log(ngx.ERR,"failed to auth: ",err)
             return
         end
     elseif err then
-        ngx.set("failed to get reused times: ", err)
+        ngx.log(ngx.ERR,"failed to get reused times: ", err)
         return
     end
 
     -- 连接池大小是100个，并且设置最大的空闲时间是 10 秒
     local ok, err = red:set_keepalive(10000, 100)
     if not ok then
-        ngx.set("failed to set keepalive: ", err)
+        ngx.log(ngx.ERR,"failed to set keepalive: ", err)
         return
     end
 
