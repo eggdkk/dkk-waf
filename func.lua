51f35c542e07191352afe714a7d4495c6d7b02d9
eggdkk <dk891896315@163.com>
2021-02-10 13:56:40 +0800

    log_5

--- func.lua
+++ func.lua
@@ -83,11 +83,12 @@ function wafLog(data, rule_tag)
     if optionIsOn(attack_log) then
         local realIp = getClientIp()
         local ua = ngx.var.http_user_agent
+        local local_time = ngx.localtime()
         local time =ngx.now()*1000
         if ua then
-            line = '{"id":'..time..',"realIp":"'..realIp .. '", "time":"' .. time .. '","request_method":"' .. request_method .. '", "url":"' .. url .. '","ua":"'.. ua ..'","data":"' .. data .. '", "ruletag":"' .. rule_tag .. '"}\n';
+            line = '{"id":'..time..',"realIp":"'..realIp .. '", "time":"' .. local_time .. '","request_method":"' .. request_method .. '", "url":"' .. url .. '","ua":"'.. ua ..'","data":"' .. data .. '", "ruletag":"' .. rule_tag .. '"}\n';
         else
-            line = '{"id":'..time..',"realIp":"'..realIp .. '", "time":"' .. time .. '","request_method":"' .. request_method .. '", "url":"' .. url .. '","data":"' .. data .. '", "ruletag":"' .. rule_tag .. '"}\n';
+            line = '{"id":'..time..',"realIp":"'..realIp .. '", "time":"' .. local_time .. '","request_method":"' .. request_method .. '", "url":"' .. url .. '","data":"' .. data .. '", "ruletag":"' .. rule_tag .. '"}\n';
         end
         local filename = log_path .. "/response.log"
         write(filename, line)
